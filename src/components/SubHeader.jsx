5b9730e4ef222425538a706800bf8c37491a1503
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-07-07 14:19:28 -0400

    Add SubHeader Component

---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,11 @@
+export default function SubHeader() {
+  return (
+    <div className="flex gap-5 justify-between px-20 py-6 text-lg font-medium text-center text-gray-500 border-b border-gray-500 border-solid max-md:flex-wrap max-md:px-5">
+      <div className="text-slate-800">Airpods</div>
+      <div>Powerbanks</div>
+      <div>Phonecase</div>
+      <div>Chargers</div>
+      <div className="max-md:max-w-full">Protective screens</div>
+    </div>
+  );
+}
